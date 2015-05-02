45d02312b0993aa1d58fcef22da74549537db4e6
marco <marco.piovan@gmail.com>
2015-05-02 11:24:20 +0200

    wrong host

--- src/main/java/cfairtestweb/client/RestClient.java
+++ src/main/java/cfairtestweb/client/RestClient.java
@@ -15,7 +15,7 @@ public class RestClient implements IRestClient{
 
 	@Override
 	public String getLastTrade() {
-		WebResource r = client.resource("http://http://cfairtest-mrcpvn.rhcloud.com/api/trade/last");
+		WebResource r = client.resource("http://cfairtest-mrcpvn.rhcloud.com/api/trade/last");
 		ClientResponse response = r.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
 		return response.getEntity(String.class);
 	}
